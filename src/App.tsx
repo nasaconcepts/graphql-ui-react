85e7413372d419d5de849cd309568df33e0fdda1
Chinasa Obichere <chinasaobichere@admins-Air.broadband>
2025-03-28 14:05:20 +0000

    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from "@apollo/client";
+import PlayersBoard from "./components/PlayersBoard";
+
+import { apolloClient } from "../src/components/ApolloClient";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div className="min-h-screen bg-gray-100 py-8">
+        <div className="max-w-4xl mx-auto px-4">
+          <PlayersBoard />
+        </div>
+      </div>
+    </ApolloProvider>
   );
 }
 
